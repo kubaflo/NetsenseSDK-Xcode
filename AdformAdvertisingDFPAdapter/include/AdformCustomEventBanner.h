1feb19f212b919be0763c24bba6dddd31426af20
Jakub Florkowski <42434498+kubaflo@users.noreply.github.com>
2022-06-10 12:50:38 +0200

    Add files via upload

---
+++ AdformAdvertisingDFPAdapter/include/AdformCustomEventBanner.h
@@ -0,0 +1,14 @@
+//
+//  AdformCustomEventBanner.h
+//  dfp-mediation
+//
+//  Created by Vladas Drejeris on 03/05/16.
+//  Copyright © 2016 adform. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface AdformCustomEventBanner : NSObject <GADCustomEventBanner>
+
+@end
